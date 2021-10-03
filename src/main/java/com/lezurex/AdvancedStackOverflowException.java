c4d8e4e7d1309288f549d44a41457e2ff15811dd
MarvinGuacamole <55056535+MarvinGuacamole@users.noreply.github.com>
2021-10-03 20:24:06 +0200

    stackoverlfow

---
+++ src/main/java/com/lezurex/AdvancedStackOverflowException.java
@@ -0,0 +1,8 @@
+package com.lezurex;
+
+public class AdvancedStackOverflowException extends StackOverflowError{
+
+    public AdvancedStackOverflowException() {
+        super("Upgrade your PC if u get this Error");
+    }
+}
