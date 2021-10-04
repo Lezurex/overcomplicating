1ab756f1373901faf182fa6b72211e03515098b2
Marvin Zeiter <marvin131t@gmail.com>
2021-10-04 13:50:43 +0200

    aaa

--- src/main/java/com/lezurex/overcomplicated/IsIntegerNegative.java
+++ src/main/java/com/lezurex/overcomplicated/IsIntegerNegative.java
@@ -1,7 +1,17 @@
+/**
+ * @author Z-100
+ */
+
 package com.lezurex.overcomplicated;
 
 public class IsIntegerNegative {
 
+    /**
+     * Method used to check if an integer is negative
+     *
+     * @param input The input integer
+     * @return the output depending on if the input integer is negative or not
+     */
     public static boolean isNegative(int input) {
         char[] numberStringArray = (input + "").
                 toCharArray();
@@ -17,10 +27,18 @@ public class IsIntegerNegative {
         }
     }
 
+    /**
+     * Method used to return true
+     * @return true
+     */
     private static boolean returnTrue() {
         return !false;
     }
 
+    /**
+     * Method used to return false
+     * @return false
+     */
     private static boolean returnFalse() {
         return !true;
     }
